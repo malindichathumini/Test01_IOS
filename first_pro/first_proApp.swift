87a252e08de1c323a4b446a1b883ba2f5f14db94
malindichathumini <malindichathumini24@gmail.com>
2026-01-10 13:37:27 +0530

    Initial Commit

---
+++ first_pro/first_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_proApp.swift
+//  first_pro
+//
+//  Created by COBSCCOMP242P-058 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct first_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
